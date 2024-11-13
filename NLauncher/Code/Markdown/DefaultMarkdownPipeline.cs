6da0a7f8033fc0b7718da1caf9f792bd2826494c
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-11-13 20:43:47 +0200

    TODO: Remove HTML from index descriptions

--- NLauncher/Code/Markdown/DefaultMarkdownPipeline.cs
+++ NLauncher/Code/Markdown/DefaultMarkdownPipeline.cs
@@ -11,11 +11,9 @@ public static class DefaultMarkdownPipeline
     private static MarkdownPipeline BuildDefaultPipeline()
     {
         return new MarkdownPipelineBuilder()
-            // !! DANGEROUS !!
-            // HTML is disabled since it was too limited (we couldn't have comments and/or colored text)
-            // This is unsafe which doesn't matter currently as this is a hobby project and we download our markdown from a trusted source.
-            // But consider changing this in the future.
-            // .DisableHtml()
+            // Disable HTML for safe parsing
+            // This does limit our styling options, but MudBlazor.Markdown doesn't support them anyways...
+            .DisableHtml()
 
             // Strikethrough is supported by VSCode markdown preview so I guess that would be a nice to have
             // I don't care about the other emphasis extras since VSCode doesn't support them.
