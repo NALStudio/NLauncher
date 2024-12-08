d97be2ef450971320499768756aa62cfba3344e4
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-12-08 16:29:49 +0200

    Finish news banner

--- NLauncher.Index/Models/News/NewsEntryAssets.cs
+++ NLauncher.Index/Models/News/NewsEntryAssets.cs
@@ -9,6 +9,7 @@ public class NewsEntryAssets
 {
     public required Uri Background { get; init; }
     public required double BackgroundBrightness { get; init; }
+    // ^^ Background Brightness requires a new index rebuild after every news asset change
 
     public required Uri Logo { get; init; }
 }
